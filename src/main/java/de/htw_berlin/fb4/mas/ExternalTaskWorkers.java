18177e3b3a8b0365d8a37f0b3f5be8262ff81f62
Alihaydar21 <alihaydar.sah@gmail.com>
2025-01-16 10:05:44 +0100

    commit 1 email

--- src/main/java/de/htw_berlin/fb4/mas/ExternalTaskWorkers.java
+++ src/main/java/de/htw_berlin/fb4/mas/ExternalTaskWorkers.java
@@ -1,10 +1,13 @@
 package de.htw_berlin.fb4.mas;
 
 import de.htw_berlin.fb4.mas.worker.PrintVariables;
+import de.htw_berlin.fb4.mas.worker.RunUiPathRobot;
 import de.htw_berlin.fb4.mas.worker.SendMail;
 import org.camunda.bpm.client.ExternalTaskClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Path;
 
 public class ExternalTaskWorkers {
 
@@ -20,6 +23,9 @@ public class ExternalTaskWorkers {
 
         client.subscribe("print-variables").handler(new PrintVariables()).open();
         client.subscribe("send-mail").handler(new SendMail()).open();
+        client.subscribe("Main")
+                .handler(new RunUiPathRobot(Path.of("C:\\Users\\aliha\\Documents\\UiPath\\Mitarbeitergewinnung\\Mitarbeitergewinnung.1.0.2.nupkg")))
+                .open();
 
         log.info("ExternalTaskWorkers started");
     }
